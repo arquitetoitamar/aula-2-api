807f5c7bd66b25490711a8911ed5ba51c546cc9f
itamar <isantos.ads@gmail.com>
2025-09-01 18:46:07 -0300

    add readme

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo apt update  
+sudo apt install postgresql-client -y #client do postgresql
+# conectar no banco
+#psql -h banco-aula-2.cdvbdsggoecu.us-east-1.rds.amazonaws.com -U postgres
+
+#CREATE EXTENSION IF NOT EXISTS vector;
