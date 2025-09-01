01695daafbcdf9d272b7fed3f77f685f11498445
itamar <isantos.ads@gmail.com>
2025-09-01 16:05:52 -0300

    fix openai

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify, request
 import psycopg
 import os
 from dotenv import load_dotenv
-import openai
 import numpy as np
 from sentence_transformers import SentenceTransformer
 from flask_cors import CORS
